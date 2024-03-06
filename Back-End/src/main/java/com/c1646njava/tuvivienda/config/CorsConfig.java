1467bfc1e80cedc05c16bacf0a98ee8851737e49
k-alex-leon <kelemon824@gmail.com>
2024-03-06 18:22:39 -0500

    Cors Config Working

--- Back-End/src/main/java/com/c1646njava/tuvivienda/config/CorsConfig.java
+++ Back-End/src/main/java/com/c1646njava/tuvivienda/config/CorsConfig.java
@@ -1,17 +1,18 @@
 package com.c1646njava.tuvivienda.config;
 
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
+@Configuration
 public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry){
 
-        registry.addMapping("/")
-                .allowedOriginPatterns("")
-                .allowedMethods("")
-                .allowedHeaders("")
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
                 .allowCredentials(false);
     }
 }
